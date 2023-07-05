9ff7bac4fcffe68f2fb712b543ce758748ffac7d
Naraly <andrea.solis@sacmex.cdmx.gob.mx>
2023-07-05 17:58:24 -0600

    feat: routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/pages/login/login.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: LoginComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  //{useHash: true} esto añadirá un # a la ruta, que es un viejo truco de los navegadores para evitar que el navegador recargue la pagina.
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
