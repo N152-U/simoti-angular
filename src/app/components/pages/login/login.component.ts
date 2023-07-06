45845bc48afe4575fee97fd45af874b2fabb4486
Naraly <andrea.solis@sacmex.cdmx.gob.mx>
2023-07-06 16:49:05 -0600

    feat: login

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { Validators, FormBuilder } from "@angular/forms";
 
+import { environment } from "../../../../environments/environment";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  siteKey: string;
+  appVersion: string;
+  appAlias: string;
+  aFormGroup: any;
+  contador = 1;
+  fieldTextType: boolean = false;
+
+  constructor(private router: Router, public formBuilder: FormBuilder) {
+    this.appVersion = environment.version;
+    this.appAlias = environment.alias;
+    this.siteKey = "6LehS1QbAAAAAOVXYZn6AnHb3YDpc64FlYMF6CL2";
+  }
+
+  ngOnInit(): void {
+    this.aFormGroup = this.formBuilder.group({
+      username: ["", [Validators.required, Validators.minLength(2)]],
+      password: ["", Validators.required],
+      recaptcha: ["", this.contador > 3 ? Validators.required : Validators.nullValidator]
+    });
+  }
+  onSubmit() {
+    //pristine es una propiedad del formulario que indica si el formulario se conserva
+    //tal cual se dio al usuario(true), si el usuario lo modifica (false)
+
+    if (this.contador > 3) {
+    } else {
+    }
+
+  }
+  get username() {
+    return this.aFormGroup.get('username');
+  }
+  get password() {
+    return this.aFormGroup.get('password');
+  }
+  get recaptcha() {
+    return this.aFormGroup.get('recaptcha');
+  }
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'http://127.0.0.1:8000',
   version: "1.0.0",
   alias: "SIMOTI ANGULAR"
