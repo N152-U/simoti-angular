24823dd4d6ecfc2b47cbb509e030dab9f712fe10
Naraly <andrea.solis@sacmex.cdmx.gob.mx>
2023-08-16 17:13:37 -0600

    feat: leer permisos

--- src/app/services/managment/manage-permissions/manage-permissions.service.ts
+++ src/app/services/managment/manage-permissions/manage-permissions.service.ts
@@ -14,23 +14,23 @@ export class ManagePermissionsService {
     return this.http.get<Permission[]>(`${environment.apiUrl}/permissions`);
   }
 
-  getTotalRegisters(offset: any, limit: any, permission = null) {
-    return this.http.get<number>(`${environment.apiUrl}/permissions`);
+  createPermission(permission: Permission): Observable<Permission> {
+    return this.http.post<Permission>(`${environment.apiUrl}/permissions/add`, permission);
   }
 
   getPermission(id: string): Observable<Permission> {
     return this.http.get<Permission>(`${environment.apiUrl}/permissions/${id}`)
   }
 
-  createPermission(permission: Permission): Observable<Permission> {
-    return this.http.post<Permission>(`${environment.apiUrl}/permissions`, permission);
-  }
-
   deletePermission(id: string): Observable<Permission> {
-    return this.http.delete<Permission>(`${environment.apiUrl}/permissions/${id}`);
+    return this.http.delete<Permission>(`${environment.apiUrl}/permissions/delete/${id}`);
+  }
+  updatePermission(id: string, permission: Permission): Observable<Permission> {
+    return this.http.put<Permission>(`${environment.apiUrl}/permissions/update/${id}`, permission);
+  }
+  getTotalRegisters(offset: any, limit: any, permission = null) {
+    return this.http.get<number>(`${environment.apiUrl}/permissions`);
   }
 
-  updatePermission(id: string, permission: Permission): Observable<Permission> {
-    return this.http.put<Permission>(`${environment.apiUrl}/permissions/${id}`, permission);
-  }
+
 }
