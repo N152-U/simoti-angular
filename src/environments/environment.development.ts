89e5e5511ef45a78072aae04077ac314b892b9e3
Andrea <naralysolis@hotmail.com>
2023-10-03 07:37:55 -0600

    fix: environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:5000/api/v1',
+  apiUrl: 'http://127.0.0.1:5000/simoti/api/v1',
   version: "1.0.0",
   alias: "SIMOTI"
 };
