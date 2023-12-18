852553541f0192fb0b9931806302ebd2f5c915e6
Naraly <andrea.solis@sacmex.cdmx.gob.mx>
2023-12-18 08:38:13 -0600

    feat: patient

--- src/app/components/pages/managment/manage-patients/manage-patients.module.ts
+++ src/app/components/pages/managment/manage-patients/manage-patients.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ManagePatientsRoutingModule } from './manage-patients-routing.module';
 import { ManagePatientsComponent } from './manage-patients.component';
+import { NewPatientComponent } from './new-patient/new-patient.component';
+import { EditPatientComponent } from './edit-patient/edit-patient.component';
+import { DetailPatientComponent } from './detail-patient/detail-patient.component';
 
 
 @NgModule({
   declarations: [
-    ManagePatientsComponent
+    ManagePatientsComponent,
+    NewPatientComponent,
+    EditPatientComponent,
+    DetailPatientComponent
   ],
   imports: [
     CommonModule,
