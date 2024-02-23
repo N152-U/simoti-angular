fc19a67f24c0203503bdb02abac41c6327b7ee68
Naraly <andrea.solis@sacmex.cdmx.gob.mx>
2024-02-22 18:02:08 -0600

    feat: craete patient

--- src/app/components/pages/managment/manage-patients/manage-patients.module.ts
+++ src/app/components/pages/managment/manage-patients/manage-patients.module.ts
@@ -1,17 +1,79 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { ManagePatientsRoutingModule } from './manage-patients-routing.module';
 import { ManagePatientsComponent } from './manage-patients.component';
 
+//primeNG
+import { AccordionModule } from "primeng/accordion"; //accordion and accordion tab
+import { TableModule } from "primeng/table";
+import { ToastModule } from "primeng/toast";
+import { CalendarModule } from "primeng/calendar";
+import { SliderModule } from "primeng/slider";
+import { MultiSelectModule } from "primeng/multiselect";
+import { ContextMenuModule } from "primeng/contextmenu";
+import { DialogModule } from "primeng/dialog";
+import { ButtonModule } from "primeng/button";
+import { DropdownModule } from "primeng/dropdown";
+import { ProgressBarModule } from "primeng/progressbar";
+import { ProgressSpinnerModule } from "primeng/progressspinner";
+import { InputTextModule } from "primeng/inputtext";
+import { TooltipModule } from "primeng/tooltip";
+import { TreeTableModule } from "primeng/treetable";
+import { BlockUIModule } from "primeng/blockui";
+import { PanelModule } from "primeng/panel";
+import { RippleModule } from "primeng/ripple";
+import { ToolbarModule } from "primeng/toolbar";
+import { InputNumberModule } from "primeng/inputnumber";
+import { CheckboxModule } from "primeng/checkbox";
+import { RadioButtonModule } from "primeng/radiobutton";
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { TabViewModule } from "primeng/tabview";
+import { FileUploadModule } from "primeng/fileupload";
+import { RatingModule } from "primeng/rating";
+import { MenuModule } from "primeng/menu";
+import { CardModule } from "primeng/card";
+import { NgxPermissionsModule } from 'ngx-permissions';
 
 @NgModule({
   declarations: [
     ManagePatientsComponent
   ],
   imports: [
+    ManagePatientsRoutingModule,
     CommonModule,
-    ManagePatientsRoutingModule
+    CardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AccordionModule,
+    TableModule,
+    ToolbarModule,
+    CalendarModule,
+    SliderModule,
+    DialogModule,
+    MultiSelectModule,
+    ContextMenuModule,
+    DropdownModule,
+    ButtonModule,
+    ToastModule,
+    InputTextModule,
+    ProgressBarModule,
+    ProgressSpinnerModule,
+    TooltipModule,
+    TreeTableModule,
+    BlockUIModule,
+    PanelModule,
+    InputNumberModule,
+    RippleModule,
+    CheckboxModule,
+    RadioButtonModule,
+    InputTextareaModule,
+    TabViewModule,
+    MenuModule,
+    FileUploadModule,
+    RatingModule,
+    NgxPermissionsModule.forChild(),
   ]
 })
 export class ManagePatientsModule { }
