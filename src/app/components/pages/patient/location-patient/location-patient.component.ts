66a2676803761f6f42138936c7fa2352c9068cbd
Naraly Solis <naralysolis@outlook.com>
2025-06-18 00:32:05 -0600

    feat: location

--- src/app/components/pages/patient/location-patient/location-patient.component.ts
+++ src/app/components/pages/patient/location-patient/location-patient.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import Chart from 'chart.js/auto';
+
 import { ActivatedRoute, Router } from '@angular/router';
 import * as moment from 'moment';
 import { ViewChild, ElementRef } from '@angular/core';
 import 'chartjs-adapter-moment';
+
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import Graphic from "@arcgis/core/Graphic";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import Point from "@arcgis/core/geometry/Point";
+import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol";
+import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer";
+import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer";
+import SimpleFillSymbol from "@arcgis/core/symbols/SimpleFillSymbol";
+
+
 
 import { PatientsService } from '@app/services/managment/patients/patients.service';
 
@@ -18,11 +30,11 @@ export class LocationPatientComponent implements OnInit {
   public chart: any;
   public indicator: any = 'Indicador de Geolocalización';
   public age: any = 25;
-
+  view: MapView | undefined;
   fechaInicio: string = '';
   fechaFin: string = '';
   public hash: string = '';
-  public location: { fecha: string, valor: string }[] = [];
+  public location: { fecha: string, valor: string, latitude:any, longitude:any }[] = [];
   public patient: any = {}
 
   constructor(private ps: PatientsService, private route: ActivatedRoute) { }
@@ -50,83 +62,79 @@ export class LocationPatientComponent implements OnInit {
 
       this.location = dataArray.map((item: any) => ({
         fecha: moment(item.created_at).format('YYYY-MM-DD HH:mm'),
-        valor: item.latitude+', '+item.longitude
+        valor: item.latitude+', '+item.longitude,
+        latitude: item.latitude,
+        longitude: item.longitude
       }));
 
-      this.createChart();
+      this.mostrarMapa();
     });
   }
+  mostrarMapa() {
+    const graphicsLayer = new GraphicsLayer();
 
+    this.location.forEach(loc => {
+      const point = new Point({
+        longitude: loc.longitude,
+        latitude: loc.latitude
+      });
 
-  createChart() {
-    if (this.chart) {
-      this.chart.destroy();
-    }
-
-    setTimeout(() => {
-      const ctx = this.myChartCanvas?.nativeElement?.getContext('2d');
-      if (!ctx) {
-        console.error('No se pudo obtener el contexto del canvas.');
-        return;
-      }
-
-      const labels = this.location.map((item) => item.fecha);
-      const data = this.location.map((item) => item.valor);
-
-      this.chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels,
-          datasets: [
-            {
-              label: 'Temperatura (°C)',
-              data,
-              borderColor: '#044599',
-              backgroundColor: 'rgba(4, 69, 153, 0.2)',
-              fill: true,
-              tension: 0.3,
-              pointRadius: 5,
-              pointHoverRadius: 7
-            }
-          ]
-        },
-        options: {
-          responsive: true,
-          aspectRatio: 2.5,
-          scales: {
-            y: {
-              beginAtZero: false,
-              suggestedMin: 35,
-              suggestedMax: 42,
-              title: {
-                display: true,
-                text: '°C'
-              }
-            },
-            x: {
-              type: 'time',
-              time: {
-                unit: 'minute',
-                displayFormats: {
-                  minute: 'MMM D HH:mm',
-                  hour: 'MMM D HH:mm'
-                },
-                tooltipFormat: 'YYYY-MM-DD HH:mm'
-              },
-              title: {
-                display: true,
-                text: 'Fecha y hora'
-              },
-            }
-          },
-          plugins: {
-            legend: {
-              display: true
-            }
-          }
+      const markerSymbol = new SimpleMarkerSymbol({
+        color: "#0079c1",
+        outline: {
+          color: "#ffffff",
+          width: 1
         }
       });
-    }, 0);
+
+      const pointGraphic = new Graphic({
+        geometry: point,
+        symbol: markerSymbol,
+        attributes: {
+          Fecha: loc.fecha
+        },
+        popupTemplate: {
+          title: "Localización",
+          content: "Fecha: {Fecha}"
+        }
+      });
+
+      graphicsLayer.add(pointGraphic);
+    });
+
+    const fillSymbol = new SimpleFillSymbol({
+      color: [0, 128, 255, 0.2], // Azul semitransparente
+      outline: {
+        color: "#004085",
+        width: 1
+      }
+    });
+    
+    const renderer = new SimpleRenderer({
+      symbol: fillSymbol
+    });
+    
+
+    const alcaldiasLayer = new GeoJSONLayer({
+      url: "assets/esri/municipalities.geojson",
+      title: "Alcaldías CDMX",
+      renderer: renderer,
+      opacity: 0.6
+    });
+    
+
+    const map = new Map({
+      basemap: "streets-navigation-vector",
+      layers: [alcaldiasLayer,graphicsLayer]
+    });
+
+   
+    this.view = new MapView({
+      container: "viewDiv",
+      map: map,
+      center: [-99.14511395269028,19.372007846101095],
+      zoom: 9
+    });
   }
 
 
